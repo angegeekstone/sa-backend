d3e5320e0ccace0c26ecf51f2eab9d16a541f218
angegeekstone <angestephanekobo@gmail.com>
2024-08-14 01:45:48 +0000

    first commit

---
+++ src/main/java/com/geekstone/sa_backend/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.geekstone.sa_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "test")
+public class TestController {
+
+    @GetMapping()
+    public List<String> getList() {
+        return List.of("ddkkd", "hsdgdg");
+    }
+
+    @GetMapping(path = "string")
+   public String getString() {
+        return "Chaine de caractere transmise";
+    }
+
+
+}
